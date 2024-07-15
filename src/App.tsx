4b66271cf2aa13ba44297408479d78c96b2c6d55
aleksand <sanyok.10rus@gmail.com>
2024-07-15 05:29:51 +0300

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage/HomePage'
+import AuthPage from './pages/AuthPage/AuthPage'
+import CardPage from './pages/CardPage/CardPage'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path={'auth'} element={<AuthPage />} />
+      <Route path={'card'} element={<CardPage />} />
+      <Route path={'*'} element={<Navigate replace to="/" />} />
+    </Routes>
   )
 }
 
