c4a7bc4adf1fba3f39dab368f9b4d3f9fe1c609c
aleksand <sanyok.10rus@gmail.com>
2024-07-17 23:11:02 +0300

    added handlers to form

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,16 +1,58 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { SvgEye } from '../../components/Svg/SvgEye';
 import './AuthPage.scss';
 
 export const AuthPage = () => {
-  const [passwordField, setPasswordField] = useState(false);
-  const [confirmPasswordField, setConfirmPasswordField] = useState(false);
+  const [passwordField, setPasswordField] = useState<boolean>(false);
+  const [confirmPasswordField, setConfirmPasswordField] = useState<boolean>(false);
+  const [confirmPasswordFieldError, setConfirmPasswordFieldError] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
 
+  const changePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const changeConfirmPassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(event.target.value);
+  };
+  const changeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const ValidateEmail = () => {
+    const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (email.match(isValidEmail)) {
+      setEmailError(false);
+    } else {
+      setEmailError(true);
+    }
+  }
+
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    if (password !== confirmPassword) {
+      setConfirmPasswordFieldError(true);
+    } else {
+      setConfirmPasswordFieldError(false);
+    }
+
+    //отправить запрос на регистрацию
+    // if (!confirmPasswordFieldError && !emailError) {
+
+    // }
+    //отправить запрос на вход
+    // if (!emailError) {
+
+    // }
+  }
 
 
   return (
     <div className="flex-center auth-page">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={e=>submitHandler(e)}>
         <h1 className="auth-title">
           Регистрация
         </h1>
@@ -18,21 +60,39 @@ export const AuthPage = () => {
           Имя
           <input type="text" placeholder="Артур" />
         </label>
-        <label htmlFor="email" className='error'>
+        <label htmlFor="email" className={emailError ? `error` : ``}>
           Электронная почта
-          <input type="text" placeholder="example@mail.ru" />
+          <input
+            type="email"
+            placeholder="example@mail.ru"
+            onChange={changeEmail}
+            onFocus={()=>setEmailError(false)}
+            onBlur={ValidateEmail}
+          />
         </label>
         <label className="pos-rel" htmlFor="password">
           Пароль
-          <input type={passwordField ? 'text' : 'password'} placeholder="******" />
+          <input
+            type={passwordField ? 'text' : 'password'}
+            placeholder="******"
+            value={password}
+            onChange={changePassword}
+            required
+          />
           <div className="show-pass" onClick={()=>setPasswordField(prev => !prev)} >
             <SvgEye />
           </div>
         </label>
-        <label className="pos-rel" htmlFor="confirm">
+        <label className={confirmPasswordFieldError ? `pos-rel error`: `pos-rel`} htmlFor="confirm">
           Подтвердите пароль
-          <input type={confirmPasswordField ? 'text' : 'password'} placeholder="******" />
-          <div className="show-pass" onClick={()=>setConfirmPasswordField(prev => !prev)} >
+          <input
+            type={confirmPasswordField ? 'text' : 'password'}
+            placeholder="******"
+            value={confirmPassword}
+            onChange={changeConfirmPassword}
+            onFocus={()=>setConfirmPasswordFieldError(false)}
+          />
+          <div className="show-pass" onClick={()=>setConfirmPasswordField(prev => !prev)}>
             <SvgEye />
           </div>
         </label>
